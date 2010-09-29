bb83b09543881f5b504d4843a52464571052f267
Lee Smith <lee.smith@appogee.net>
2010-09-28 23:25:39 -0500

    Added more i8n translations on sign in and dashboard pages

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,17 +9,18 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      msg = "Welcome back <strong>#{@user_session.user.username}</strong>!"
+      #msg = "Welcome <strong>#{@user_session.user.username}</strong>!"
+      msg = t('welcome_on_login', :username => @user_session.user.username)
       if @user_session.user.last_login_at
-        msg = msg + "&nbsp;Your last login was on <strong>#{@user_session.user.last_login_at.strftime("%Y-%m-%d %I:%M %p")}</strong> from <strong>#{@user_session.user.last_login_ip}</strong>"
+        msg = msg + "&nbsp;" + t('login_details', :timestamp => @user_session.user.last_login_at.strftime("%Y-%m-%d %I:%M %p"), :ip => @user_session.user.last_login_ip)
       end
       flash[:success] = msg
       redirect_back_or_default dashboard_index_path
     else
       if @user_session.being_brute_force_protected?
-        flash[:error] = "User is locked: exceeded failed login limit!"
+        flash[:error] = t('locked_out')
       else
-        flash[:error] = "Invalid login attempt!"
+        flash[:error] = t('invalid_signin')
       end
       redirect_to login_path
     end
@@ -27,7 +28,7 @@ class UserSessionsController < ApplicationController
 
   def destroy
     current_user_session.destroy
-    flash[:success] = "Logout successful!"
+    flash[:success] = t('logout')
     redirect_back_or_default login_path
   end
 
