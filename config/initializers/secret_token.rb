a74ff85f3d640e4ab0e268126ea94e5104b326a9
Lee Smith <jesmith@temenos.com>
2014-03-07 11:03:52 -0600

    Fresh rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TicketMule::Application.config.secret_key_base = 'f2448a02580c76de76d9dac8359c0f0734e73b5429db0dce863085765830db081a51557daf91d94faad289dded2413cf1fcbb636bbf8bc2cbf2c9e7a29bc0f57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TicketMule::Application.config.session_store :cookie_store, key: '_ticket_mule_session'
