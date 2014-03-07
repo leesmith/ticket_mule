91dead34071e42a39f77229d806a3bd8b1730db1
Lee Smith <jesmith@temenos.com>
2014-03-07 11:01:30 -0600

    Clean slate

--- config/initializers/paperclip.rb
+++
@@ -1,3 +0,0 @@
-Paperclip.interpolates :ticket_id do |attachment, style|
-  attachment.instance.ticket_id
-end
--- public/javascripts/pagination.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $('.pagination a').live("click", function () {
-    //$(".pagination").html('Page is loading...');
-    //$(".loading").css('display', 'block').fadeOut(600);
-    $.get(this.href, null, null, 'script');
-    return false;
-  });
-});
